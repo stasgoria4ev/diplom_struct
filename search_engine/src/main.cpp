36f0fddb8ef4c85aa7ce57f5c0794a3a6c5e3aa5
Stanislav Goriachev <stas.goria4ev@mail.ru>
2024-03-12 21:43:39 +0400

    nlohmann library exceptions

--- search_engine/src/main.cpp
+++ search_engine/src/main.cpp
@@ -18,19 +18,45 @@ int main() {
         std::cerr << "Cought exception: " << x.what() << '\n';
         exit(1);
     }
+    catch (const nlohmann::json::parse_error& x) {
+        std::cerr << "Exception found in config.json: " << x.what() << '\n';
+        exit(1);
+    }
+    catch (const nlohmann::json::type_error& y) {
+        std::cerr << "Exception found in config.json: " << y.what() << '\n';
+        exit(1);
+    }
+    catch (...) {
+        std::cerr << "Some exception occurred in config.json...\n";
+        exit(1);
+    }
 
     invertedIndex.max_responses = ConverterJSON::GetResponsesLimit();//проброс max_responses в SearchServer через _index(idx)
 
     SearchServer searchServer(invertedIndex);
 
-    std::vector<std::vector<RelativeIndex>> searchServerSearch = searchServer.search(converterJSON.GetRequests());
-    std::vector<std::vector<std::pair<int, float>>> answersTemp;
-    for (auto & i : searchServerSearch) {
-        std::vector<std::pair<int, float>> answersTempJ;
-        for (auto & j : i) {
-            answersTempJ.emplace_back(std::pair(j.doc_id, j.rank));
+    try {
+        std::vector<std::vector<RelativeIndex>> searchServerSearch = searchServer.search(converterJSON.GetRequests());
+        std::vector<std::vector<std::pair<int, float>>> answersTemp;
+        for (auto &i: searchServerSearch) {
+            std::vector<std::pair<int, float>> answersTempJ;
+            for (auto &j: i) {
+                answersTempJ.emplace_back(std::pair(j.doc_id, j.rank));
+            }
+            answersTemp.push_back(answersTempJ);
         }
-        answersTemp.push_back(answersTempJ);
+        converterJSON.putAnswers(answersTemp);
     }
-    converterJSON.putAnswers(answersTemp);
+    catch (const nlohmann::json::parse_error& x) {
+        std::cerr << "Exception found in requests.json: " << x.what() << '\n';
+        exit(1);
+    }
+    catch (const nlohmann::json::type_error& y) {
+        std::cerr << "Exception found in requests.json: " << y.what() << '\n';
+        exit(1);
+    }
+    catch (...) {
+        std::cerr << "Some exception occurred in requests.json...\n";
+        exit(1);
+    }
 }
